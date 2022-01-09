1d06e8ce9f6992868bc6b52033b7c80a7487815f
wingio <mcprowing@gmail.com>
2022-01-08 20:05:53 -0500

    Punctiuation

--- commands/god.ts
+++ commands/god.ts
@@ -1,12 +1,12 @@
 import { Client, Message } from "discord.js";
 import Command from "./Command";
 
-export default class Sync extends Command {
+export default class God extends Command {
     public static commandName: string = "god";
     public static description: string = "God";
     public static aliases: string[] = ["worship"];
 
     public static run(client: Client, msg: Message, args: string[]) : void {
-        msg.channel.send("For I am the lord your God");
+        msg.channel.send("For I am the lord your God.");
     }
 }
